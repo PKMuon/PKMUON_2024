a58149c13d6177adbd14f10346314e3aacf4a111
lyazj <seeson@pku.edu.cn>
2024-10-18 11:53:56 +0800

    Update clfv scripts

--- analysis/clfv-analysis.sh
+++ analysis/clfv-analysis.sh
@@ -8,6 +8,7 @@ fi
 MAC="$1"
 
 NPROC=$(nproc || sysctl -n hw.logicalcpu || getconf _NPROCESSORS_ONLN)
+grep -q ihep.ac.cn <<< "${HOSTNAME}" && NPROC="$(bc <<< "${NPROC}*0.75/1")"
 IPROC=0
 PIDS=()
 ROOTFILE="$(dirname "${MAC}")/$(grep /rlt/SetFileName "${MAC}" | awk '{print $2;}')"
@@ -30,8 +31,11 @@ for ROOTFILE in $(ls -v ${ROOTFILES}); do
         export NUMEXPR_NUM_THREADS=1
         ROOTBASE="$(basename "${ROOTFILE}")"
         RECOFILE="${ROOTDIR}/reco_${ROOTBASE}"
+        RENEFILE="${ROOTDIR}/rene_${ROOTBASE}"
         echo ./clfv-reco.py "${ROOTFILE}" -o "${RECOFILE}"
-        ./clfv-reco.py "${ROOTFILE}" -o "${RECOFILE}" &> "${RECOFILE}.log"
+        echo ./clfv-reco.py "${ROOTFILE}" -o "${RENEFILE}" -e 1.0
+        [ -f "${RECOFILE}" ] || ./clfv-reco.py "${ROOTFILE}" -o "${RECOFILE}" &> "${RECOFILE}.log"
+        [ -f "${RENEFILE}" ] || ./clfv-reco.py "${ROOTFILE}" -o "${RENEFILE}" -e 1.0 &> "${RENEFILE}.log"
     ) &
     PIDS=(${PIDS[@]} $!)
 done
@@ -39,9 +43,14 @@ wait
 (
     RECOFILE="${ROOTDIR}/reco_${ROOTBASE}"
     RECOFILES="${RECOFILE/.root/_*.root}"
-    RECOPLOT="${RECOFILE/.root/.pdf}"
-    hadd -f "${RECOFILE}" $(ls -v ${RECOFILES})
+    RENEFILE="${ROOTDIR}/rene_${ROOTBASE}"
+    RENEFILES="${RENEFILE/.root/_*.root}"
+    hadd -f "${RECOFILE}" $(ls -v ${RECOFILES}) &
+    hadd -f "${RENEFILE}" $(ls -v ${RENEFILES}) &
+    wait
     echo ./clfv-reco-draw.py "${RECOFILE}"
     ./clfv-reco-draw.py "${RECOFILE}"
+    echo ./clfv-reco-draw.py "${RENEFILE}"
+    ./clfv-reco-draw.py "${RENEFILE}"
 ) &
 wait
