26ae0da15a69889b29f3ea48601d3655460a999b
lyazj <seeson@pku.edu.cn>
2024-10-24 11:39:37 +0800

    Avoid memory issue

--- lxlogin_analysis_submit.sh
+++ lxlogin_analysis_submit.sh
@@ -10,6 +10,6 @@ set -ev
 
 N=$(head -1 lxlogin_run.txt)
 
-hep_sub -m 16384 lxlogin_analysis.sh -argu %{ProcId} "${POSTFIX}" \
+hep_sub lxlogin_analysis.sh -mem 6000 -argu %{ProcId} "${POSTFIX}" \
     -n $[($(wc -l lxlogin_run.txt | egrep -o '[0-9]+')-1)*${N}] \
     -o lxlogin_analysis_1.log -e lxlogin_analysis_2.log
