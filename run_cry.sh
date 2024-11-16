4852f9aae3f9f80438cbf106d17e610dc73b1003
lyazj <seeson@pku.edu.cn>
2024-11-17 01:47:00 +0800

    Update run scripts

--- run_cry.sh
+++ run_cry.sh
@@ -7,19 +7,25 @@ PIDS=()
 mkdir -p root_file
 
 # 循环运行Geant4模拟，传入不同的i值
-for I in $(seq 0 $[N-1]); do
-    if [ $IPROC = $NPROC ]; then
-        wait $PIDS
-        PIDS=(${PIDS[@]:1})
-    else
-        let IPROC+=1
-    fi
-    cp CryMu.mac CryMu_$I.mac
-    sed -i "s/CryMu\\.root/CryMu_${I}.root/g" CryMu_$I.mac
+for DM_MASS in 1.0e-02 1.0e-01 1.0e+00 1.0e+01 1.0e+02; do
+    XS=$(python3 -c "print('%.1e' % (${DM_MASS} * 1.0e-03))")
+    for I in $(seq 0 $[N-1]); do
+        if [ $IPROC = $NPROC ]; then
+            wait $PIDS
+            PIDS=(${PIDS[@]:1})
+        else
+            let IPROC+=1
+        fi
+        MACFILE="CryMu_DM_${DM_MASS}GeV_XS_${XS}cm2_${I}.mac"
+        ROOTFILE="CryMu_DM_${DM_MASS}GeV_XS_${XS}cm2_${I}.root"
+        cp CryMu.mac "root_file/${MACFILE}"
+        sed -i "s/CryMu\\.root/${ROOTFILE}/g" "root_file/${MACFILE}"
+        sed -i "s@/scatter/DM.*@/scatter/DM ${DM_MASS} GeV ${XS} cm2@g" "root_file/${MACFILE}"
 
-    # 运行Geant4模拟
-    echo ./muPos CryMu_$I.mac
-    ./muPos CryMu_$I.mac &> root_file/CryMu_${I}.root.log &
-    PIDS=(${PIDS[@]} $!)
+        # 运行Geant4模拟
+        echo ./muPos "root_file/${MACFILE}"
+        ./muPos "root_file/${MACFILE}" &> "root_file/${ROOTFILE}.log" &
+        PIDS=(${PIDS[@]} $!)
+    done
 done
 wait
