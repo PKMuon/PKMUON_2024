7247f87b89da2e45c4fd8e0fdf97ebb83c4fe09e
lyazj <seeson@pku.edu.cn>
2025-06-13 21:56:40 +0800

    Update mac files and generator

--- scan_muemumu_graphite.py
+++ scan_muemumu_graphite.py
@@ -36,7 +36,6 @@ mac_template = '''
 /gps/particle mu+
 /gps/direction 0.0 0.0 1.0
 /gps/totalEnergy {muon_energy} GeV
-/gps/position 0.0 0.0 -700.0 mm
 
 # ----------------------------------------------
 # run
