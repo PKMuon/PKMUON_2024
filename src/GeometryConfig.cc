c0828446a054658153763cbe1f90a5a729821188
lyazj <seeson@pku.edu.cn>
2025-06-13 11:55:34 +0200

    Update geometry

--- src/GeometryConfig.cc
+++ src/GeometryConfig.cc
@@ -72,20 +72,24 @@ G4Element *ParseElement(const string &name)
   return element;
 }
 
-G4LogicalVolume *ProcessBox(const string &name, YAML::Node node)
+G4LogicalVolume *ProcessBox(const string &name, YAML::Node node, unordered_map<G4LogicalVolume *, size_t> &compressible)
 {
   G4double x = ParseAbsolutePhysicsVariable(node["x"].as<string>());
   G4double y = ParseAbsolutePhysicsVariable(node["y"].as<string>());
   G4double z = ParseAbsolutePhysicsVariable(node["z"].as<string>());
   G4Material *material = ParseMaterial(node["material"].as<string>());
-  return CreateBoxVolume(name, x / 2, y / 2, z / 2, material);
+  G4LogicalVolume *volume = CreateBoxVolume(name, x / 2, y / 2, z / 2, material);
+  if(node["mid_to_mid"]) compressible[volume] = 0;
+  return volume;
 }
 
-vector<G4LogicalVolume *> ProcessStackComponents(
-    YAML::Node node, G4double &hx_s, G4double &hy_s, G4double &hz_s, G4double &hx_m, G4double &hy_m, G4double &hz_m)
+vector<G4LogicalVolume *> ProcessStackComponents(YAML::Node node, G4double &hx_s, G4double &hy_s, G4double &hz_s,
+    G4double &hx_m, G4double &hy_m, G4double &hz_m, char axis, unordered_map<G4LogicalVolume *, size_t> &compressible)
 {
   hx_s = hy_s = hz_s = 0.0;
   hx_m = hy_m = hz_m = 0.0;
+  G4double hx_ll = -1, hy_ll = -1, hz_ll = -1, hx_l = -1, hy_l = -1, hz_l = -1, hx = -1, hy = -1, hz = -1;
+  G4Box *box_to_compress = NULL;
   vector<G4LogicalVolume *> children;
   children.reserve(node["components"].size());
   for(YAML::Node child_name : node["components"]) {
@@ -99,13 +103,40 @@ vector<G4LogicalVolume *> ProcessStackComponents(
       G4cerr << "ERROR: expect box component" << G4endl;
       exit(EXIT_FAILURE);
     }
+    hx = box->GetXHalfLength(), hy = box->GetYHalfLength(), hz = box->GetZHalfLength();
+    if(hx_ll >= 0) {
+      hx_s += hx_ll, hy_s += hy_ll, hz_s += hz_ll;
+      hx_m = max(hx_m, hx_ll), hy_m = max(hy_m, hy_ll), hz_m = max(hz_m, hz_ll);
+    }
+    if(box_to_compress && hx_ll >= 0) {
+      G4double dhx = (hx_ll + hx) / 2, dhy = (hy_ll + hy) / 2, dhz = (hz_ll + hz) / 2;
+      switch(axis) {
+      case 'x': hx_l -= dhx, box_to_compress->SetXHalfLength(hx_l); break;
+      case 'y': hy_l -= dhy, box_to_compress->SetYHalfLength(hy_l); break;
+      case 'z': hz_l -= dhz, box_to_compress->SetZHalfLength(hz_l); break;
+      }
+      box_to_compress = NULL;
+    }
+    auto it = compressible.find(child);
+    if(it != compressible.end()) {
+      string name = child->GetName() + "_" + to_string(it->second++);
+      box = new G4Box(name, hx, hy, hz);
+      const G4VisAttributes *attr = child->GetVisAttributes();
+      child = new G4LogicalVolume(box, child->GetMaterial(), name);
+      child->SetVisAttributes(*attr);
+      box_to_compress = box;
+    }
+    hx_ll = hx_l, hy_ll = hy_l, hz_ll = hz_l;
+    hx_l = hx, hy_l = hy, hz_l = hz;
     children.push_back(child);
-    hx_s += box->GetXHalfLength();
-    hy_s += box->GetYHalfLength();
-    hz_s += box->GetZHalfLength();
-    hx_m = max(hx_m, box->GetXHalfLength());
-    hy_m = max(hy_m, box->GetYHalfLength());
-    hz_m = max(hz_m, box->GetZHalfLength());
+  }
+  if(hx_ll >= 0) {
+    hx_s += hx_ll, hy_s += hy_ll, hz_s += hz_ll;
+    hx_m = max(hx_m, hx_ll), hy_m = max(hy_m, hy_ll), hz_m = max(hz_m, hz_ll);
+  }
+  if(hx_l >= 0) {
+    hx_s += hx_l, hy_s += hy_l, hz_s += hz_l;
+    hx_m = max(hx_m, hx_l), hy_m = max(hy_m, hy_l), hz_m = max(hz_m, hz_l);
   }
   return children;
 }
@@ -156,10 +187,11 @@ void ProcessOffset(
   }
 }
 
-G4LogicalVolume *ProcessBottomUp(const string &name, YAML::Node node)
+G4LogicalVolume *ProcessBottomUp(
+    const string &name, YAML::Node node, unordered_map<G4LogicalVolume *, size_t> &compressible)
 {
   G4double hx_s, hy_s, hz_s, hx_m, hy_m, hz_m;
-  auto children = ProcessStackComponents(node, hx_s, hy_s, hz_s, hx_m, hy_m, hz_m);
+  auto children = ProcessStackComponents(node, hx_s, hy_s, hz_s, hx_m, hy_m, hz_m, 'z', compressible);
   G4double hx = hx_m, hy = hy_m, hz = hz_s;
   size_t duplicate = 1;
   if(node["duplicate"]) { duplicate = node["duplicate"].as<size_t>(); }
@@ -181,10 +213,11 @@ G4LogicalVolume *ProcessBottomUp(const string &name, YAML::Node node)
   return logical;
 }
 
-G4LogicalVolume *ProcessLeftRight(const string &name, YAML::Node node)
+G4LogicalVolume *ProcessLeftRight(
+    const string &name, YAML::Node node, unordered_map<G4LogicalVolume *, size_t> &compressible)
 {
   G4double hx_s, hy_s, hz_s, hx_m, hy_m, hz_m;
-  auto children = ProcessStackComponents(node, hx_s, hy_s, hz_s, hx_m, hy_m, hz_m);
+  auto children = ProcessStackComponents(node, hx_s, hy_s, hz_s, hx_m, hy_m, hz_m, 'x', compressible);
   G4double hx = hx_s, hy = hy_m, hz = hz_m;
   size_t duplicate = 1;
   if(node["duplicate"]) { duplicate = node["duplicate"].as<size_t>(); }
@@ -358,6 +391,7 @@ G4Element *ProcessElement(const string &name, YAML::Node)
 }  // namespace
 
 unordered_map<string, G4VisAttributes> GeometryConfig::fMaterialVisAttributes;
+unordered_map<G4LogicalVolume *, size_t> GeometryConfig::fCompressibleVolumes;
 
 GeometryConfig::GeometryConfig(const char *path)
 {
@@ -387,11 +421,11 @@ void GeometryConfig::ProcessVolumes()
     G4cout << "Building volume " << name << G4endl;
     G4LogicalVolume *logical;
     if(node["solid"].as<string>() == "box") {
-      logical = ProcessBox(name.as<string>(), node);
+      logical = ProcessBox(name.as<string>(), node, fCompressibleVolumes);
     } else if(node["solid"].as<string>() == "bottom_up") {
-      logical = ProcessBottomUp(name.as<string>(), node);
+      logical = ProcessBottomUp(name.as<string>(), node, fCompressibleVolumes);
     } else if(node["solid"].as<string>() == "left_right") {
-      logical = ProcessLeftRight(name.as<string>(), node);
+      logical = ProcessLeftRight(name.as<string>(), node, fCompressibleVolumes);
     } else if(node["solid"].as<string>() == "rotation") {
       logical = ProcessRotation(name.as<string>(), node);
     } else {
